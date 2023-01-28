ae423c94c2032de951dc6aba42bb3428f168c27d
AnastasiiaKliusevich <darina9220@gmail.com>
2023-01-28 21:11:04 +0300

    Add program

--- Work/Program.cs
+++ Work/Program.cs
@@ -6,7 +6,43 @@ namespace Work
     {
         static void Main(string[] args)
         {
-            
+            string[] array = { "hello", "2", "word", ":-)", "1234", "1567", "-2" };
+
+            PrintArray(FindItems(array));
+        }
+
+        static string[] FindItems(string[] array)
+        {
+            int count = 0;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                {
+                    count++;
+                }
+            }
+
+            string[] resultArray = new string[count];
+            count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                {
+                    resultArray[count] = array[i];
+                    count++;
+                }
+            }
+            return resultArray;
+        }
+
+        static void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
         }
     }
 }
